0c8e36d203fc667c9c6242fda688d763d474a894
Yang <ji3g420132@yahoo.com.tw>
2022-10-25 16:48:35 +0800

    day9~day12

--- 11 - Custom Video Player/scripts-FINISHED.js
+++ 11 - Custom Video Player/scripts-FINISHED.js
@@ -9,6 +9,8 @@ const ranges = player.querySelectorAll('.player__slider');
 
 /* Build out functions */
 function togglePlay() {
+  //三元運算 + 中括號取值
+  //影片操作 & 畫面操作 => 分開
   const method = video.paused ? 'play' : 'pause';
   video[method]();
 }
@@ -25,6 +27,7 @@ function skip() {
 
 function handleRangeUpdate() {
   video[this.name] = this.value;
+  // video.volume, video.playbackRate
 }
 
 function handleProgress() {
@@ -44,12 +47,16 @@ video.addEventListener('pause', updateButton);
 video.addEventListener('timeupdate', handleProgress);
 
 toggle.addEventListener('click', togglePlay);
+
+// 使用forEach() 讓以後新增相同功能按鈕不須改動程式架構
 skipButtons.forEach(button => button.addEventListener('click', skip));
+
 ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
 ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
 
 let mousedown = false;
 progress.addEventListener('click', scrub);
+// &&前面成立才會做後面 ||前面不成立才會做後面的 用來判斷，使程式更精簡
 progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
 progress.addEventListener('mousedown', () => mousedown = true);
 progress.addEventListener('mouseup', () => mousedown = false);
